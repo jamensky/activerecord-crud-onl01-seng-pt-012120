9f6106ae18413cf03ee2c4fb45ce91404a6f151e
Jamal Rjoul <jamensky1991@gmail.com>
2020-05-13 10:30:10 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -74,8 +74,8 @@ def can_update_multiple_items_at_once
 end
 
 def can_destroy_a_single_item
-  Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
+  movie = Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
+
   __
 end
 
